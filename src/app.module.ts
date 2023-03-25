03114a233e5cfed667d7452231022eaac5797a94
Luis Andino <landinom@unah.hn>
2023-03-25 15:47:04 -0600

    BD - BACKEND: FIX arreglo archivo env faltante

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 
+
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}), MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
