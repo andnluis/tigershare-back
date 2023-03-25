7944f294d6993836aecf64caaa627e3f182de859
Luis Andino <landinom@unah.hn>
2023-03-25 16:16:48 -0600

    uri exposed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [UsuarioModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}), MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [UsuarioModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}), MongooseModule.forRoot(process.env.DB_URI), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
