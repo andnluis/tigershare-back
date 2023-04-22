2bbf65891dacbbce10b3ff197152c9d7971dbfb1
Luis Andino <landinom@unah.hn>
2023-04-21 18:46:37 -0600

    cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProyectoModule } from './proyecto/proyecto.module';
+import { cors } from 'cors';
 
 
 @Module({
@@ -12,4 +13,10 @@ import { ProyectoModule } from './proyecto/proyecto.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(cors()).forRoutes({ path: '*', method:RequestMethod.ALL})
+  }
+
+}
