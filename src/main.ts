27fb7b6ae4b7d6aebfae73efc0709e9d5ad14f4a
Luis Andino <landinom@unah.hn>
2023-04-21 19:23:15 -0600

    cambio

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   app.enableCors({
     origin: ['http://localhost:4200','https://tigershare.vercel.app'],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
   });
+  await app.listen(3000);
 }
 bootstrap();
